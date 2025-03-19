b6b57034d6a8970d20f8676925648c37298a845a
Miguel-Lopez-06 <vxleugim@gmail.com>
2025-03-19 20:21:06 +0800

    update

--- dashboard_main.py
+++ dashboard_main.py
@@ -673,21 +673,21 @@ elif  st.session_state.page_selection == "prediction":
     if show_classes:
         # Diabetes Samples
         st.subheader("No Diabetes Samples")
-        st.dataframe(Diabetes_samples, use_container_width=True, hide_index=True)
+        st.dataframe(No_Diabetes_samples, use_container_width=True, hide_index=True)
 
         # No Diabetes Samples
         st.subheader("Diabetes Samples")
-        st.dataframe(No_Diabetes_samples, use_container_width=True, hide_index=True)
+        st.dataframe(Diabetes_samples, use_container_width=True, hide_index=True)
 
     if show_Diabetes:
         # Display the Diabetes samples
-        st.subheader("Diabetes Samples")
-        st.dataframe(Diabetes_samples, use_container_width=True, hide_index=True)
+        st.subheader("No Diabetes Samples")
+        st.dataframe(No_Diabetes_samples, use_container_width=True, hide_index=True)
 
     if show_No_Diabetes:
         # Display the No Diabetes samples
-        st.subheader("No Diabetes Samples")
-        st.dataframe(No_Diabetes_samples, use_container_width=True, hide_index=True)
+        st.subheader("Diabetes Samples")
+        st.dataframe(Diabetes_samples, use_container_width=True, hide_index=True)
 
 # Conclusions Page
 elif st.session_state.page_selection == "conclusion":
