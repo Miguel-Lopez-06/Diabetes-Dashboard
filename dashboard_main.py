43ddbe31702a314783a11fff94dbcf056bcec206
Miguel-Lopez-06 <vxleugim@gmail.com>
2025-03-19 19:51:43 +0800

    update

--- dashboard_main.py
+++ dashboard_main.py
@@ -629,6 +629,17 @@ elif  st.session_state.page_selection == "prediction":
 
          # Input boxes for the features
 
+        default_values = {
+        "Pregnancies": 1,
+        "Glucose": 85,
+        "BloodPressure": 66,
+        "SkinThickness": 29,
+        "Insulin": 0,
+        "BMI": 26.6,
+        "DiabetesPedigreeFunction": 0.351,
+        "Age": 31
+        }
+
         log_Pregnancies = st.number_input('Pregnancies', min_value=0, max_value=20, step=1, key='log_Pregnancies', 
                                   value=0 if st.session_state.clear else st.session_state.get('log_Pregnancies', 0))
 
